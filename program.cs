55610c5ec66cfd7fb69fdf674e536ca0b30f810c
rzamirzayev <118839663+rzamirzayev@users.noreply.github.com>
2022-12-19 16:07:25 +0400

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,5 +1,5 @@
 using class_task;
-student student1= new student("Rza", "Mirzeyev",99,"695.21");
+student student1= new student("Rza", "Mirzeyev", 100, "695.21");
 student1.Person();
-student1.gostericiler();
+student1.Gostericiler();
 student1.Exam();
